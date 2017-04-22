46c10320e56746d7b274f857a1c1de4e47536133
Rupsha <rupsha.bagchi@gmail.com>
2017-04-22 16:22:22 +0300

    added images

--- frontend/src/views/ProductList.js
+++ frontend/src/views/ProductList.js
@@ -21,21 +21,21 @@ export default class ProductList extends Component {
         <Link to="/products">
 
           <h1>Plant your personal tree</h1>      </Link>
-          <img src="/dummy/tree.png" alt="dummy" />
+          <img src="/dummy/plant.png" alt="dummy" />
           <p>Plant a tree in someone's name - a unique and green gift for events such as birthdays, holidays, graduations, weddings, new births, anniversaries, or any special occasion. Now you can pay tribute to your friends and loved ones while replanting our nation's forests.</p>
         </Card>
 
         <Card background='#9fc74f'>
         <Link to="/products">
           <h1>Establish a beehive</h1></Link>
-          <img src="/dummy/tree.png" alt="dummy" />
+          <img src="/dummy/beehive.png" alt="dummy" />
           <p>After 3 months your personal honey will arrive at your door</p>
         </Card>
 
         <Card background='#d9d01b'>
         <Link to="/location">
           <h1>Reduce your CO2 footprint</h1></Link>
-          <img src="/dummy/tree.png" alt="dummy" />
+          <img src="/dummy/co2.png" alt="dummy" />
           <p>Sponsor a tree to protect it from chopping</p>
         </Card>
 
