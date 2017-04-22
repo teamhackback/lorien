244e62b01f2f145eeef66163a5de9d74a0528de2
Sebastian Wilzbach <seb@wilzba.ch>
2017-04-23 02:28:47 +0300

    improve google maps

--- frontend/src/views/LocationPicker.js
+++ frontend/src/views/LocationPicker.js
@@ -4,13 +4,28 @@ import menuTitleStore from '../MenuTitleStore';
 import Dimensions from 'react-dimensions'
 import { Link } from 'react-router-dom';
 import Button from '../components/Button';
+import locations from '../Locations';
 
 import {
   withGoogleMap,
   GoogleMap,
   Marker,
+	Polygon,
+	InfoWindow,
 } from "react-google-maps";
 import SearchBox from "react-google-maps/lib/places/SearchBox";
+
+function getCircle(marker) {
+	 const magnitude = 15;
+   return {
+     path: google.maps.SymbolPath.CIRCLE,
+     fillColor: marker.selected ? 'red' : 'orange',
+     fillOpacity: .2,
+     scale: magnitude,
+     strokeColor: 'white',
+     strokeWeight: .5
+   };
+}
 
 const INPUT_STYLE = {
   boxSizing: `border-box`,
@@ -29,10 +44,11 @@ const INPUT_STYLE = {
 	animationDuration: '700ms',
 };
 
-const Map = withGoogleMap(props => (
+const Map = withGoogleMap(props => {
+	return (
   <GoogleMap
     ref={props.onMapMounted}
-    defaultZoom={7}
+    defaultZoom={5}
     center={props.center}
     onBoundsChanged={props.onBoundsChanged}
     panControl={false}
@@ -51,22 +67,35 @@ const Map = withGoogleMap(props => (
       onPlacesChanged={props.onPlacesChanged}
       inputPlaceholder="Type a name"
       inputStyle={INPUT_STYLE}
-      inputClassName="animated slideInDown"
+      inputClassName="animated slideInDown searchBox"
     />
-    {props.markers.map((marker, index) => (
-      <Marker position={marker.position} key={index} />
-    ))}
+    {props.markers.map((marker, index) =>
+      <Marker position={marker.position} key={index} title={marker.title} onClick={() => props.onMarkerClicked(marker, index)} icon={getCircle(marker)}>
+				{ marker.showInfo  && (
+          <InfoWindow onCloseClick={() => props.onMarkerClosed(marker, index)}>
+            <div>{marker.title}</div>
+          </InfoWindow>
+				)}
+			</Marker>
+			)}
+
+    {props.locations.map((location, index) => (
+      <Polygon path={location.path} key={index} />
+		))}
   </GoogleMap>
-));
+)});
 
 export default Dimensions()(class LocationPicker extends Component {
+
+  static contextTypes = { router: React.PropTypes.object }
   state = {
     bounds: null,
     center: {
-      lat: 62.120518,
-      lng: 25.144944
+      lat: 65.318541,
+      lng: 26.715859
     },
-    markers: [],
+    markers: locations,
+		hasSelectedMarker: false
   }
   componentWillMount() {
     menuTitleStore.title = "Where should we put it?";
@@ -96,13 +125,61 @@ export default Dimensions()(class LocationPicker extends Component {
     }));
 
     // Set markers; set map center to first search result
-    const mapCenter = markers.length > 0 ? markers[0].position : this.state.center;
+    //const mapCenter = markers.length > 0 ? markers[0].position : this.state.center;
+		const mapCenter = this.state.center;
 
     this.setState({
       center: mapCenter,
       markers,
     });
-  }
+  };
+
+	onMarkerClicked = (marker, i) => {
+		console.log(marker);
+		const markers = this.state.markers.map(marker => {
+			marker.showInfo = false;
+			return marker;
+		});
+
+		markers[i].showInfo = !markers[i].showInfo;
+		this.selectMarker(i);
+		this.setState({
+			markers
+		});
+	};
+
+	onMarkerClosed = (marker, i) => {
+		const markers = this.state.markers;
+		markers[i].showInfo = false;
+		this.setState({
+			markers
+		});
+	};
+
+	selectMarker = (i) => {
+		const markers = this.state.markers.map(marker => {
+			marker.selected = false;
+			return marker;
+		});
+		markers[i].selected = true;
+		markers[i].showInfo = true;
+		this.setState({
+			markers
+		});
+	}
+
+	onButtonClicked = () => {
+		if (this.state.hasSelectedMarker) {
+      this.context.router.history.push("/order/extraservices");
+			return;
+		} else {
+			this.selectMarker(Math.floor(Math.random() * 5));
+			this.setState({
+				hasSelectedMarker: true
+			});
+		}
+	}
+
   render() {
     const totalHeight = window.innerHeight - 46;
     return (
@@ -124,7 +201,10 @@ export default Dimensions()(class LocationPicker extends Component {
             onSearchBoxMounted={this.handleSearchBoxMounted}
             bounds={this.state.bounds}
             onPlacesChanged={this.handlePlacesChanged}
-            markers={this.state.markers}
+						onMarkerClicked={this.onMarkerClicked}
+						onMarkerClosed={this.onMarkerClosed}
+            markers={locations}
+						locations={[]}
           />
         </div>
         <div style={{
@@ -134,18 +214,17 @@ export default Dimensions()(class LocationPicker extends Component {
           zIndex: 10,
           width: window.innerWidth,
         }}>
-          <Link to="/order/extraservices">
             <Button
-              title="Choose for me"
+							onClick={this.onButtonClicked}
+							title={this.state.hasSelectedMarker ? "Continue" : "Choose for me"}
               className="animated slideInUp"
               style={{
-							animationDuration: '700ms',
-							animationDelay: "300ms",
-              width: 250,
-              margin: "0 auto"
+								animationDuration: '700ms',
+								animationDelay: "300ms",
+              	width: 250,
+              	margin: "0 auto"
             }}>
             </Button>
-          </Link>
         </div>
       </div>
     )
