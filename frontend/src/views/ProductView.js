b177babd0225c9c2008ee2677f6022c5c53c7de7
Sebastian Wilzbach <seb@wilzba.ch>
2017-04-22 12:41:40 +0300

    Style tile view

--- frontend/src/views/ProductView.js
+++ frontend/src/views/ProductView.js
@@ -7,12 +7,17 @@ function ResponsiveTile(props) {
       width: "100%",
       padding: 0,
       textAlign: "center",
-      height: 200
+      height: 200,
+      backgroundColor: "#d8d8d8",
+      ...props.data.style
     }}>
+      <img style={{
+        marginTop: 50,
+      }}
+      src="/dummy/tree.png" alt="dummy" />
       <div style={{
-        marginTop: 160,
         fontSize: 20,
-        display: "inline-block"
+        marginTop: 20,
       }}>
         Honey
       </div>
@@ -22,17 +27,41 @@ function ResponsiveTile(props) {
 
 const tilesData = [
   {
-    id: 1
+    id: 1,
+    style: {
+      opacity: 1
+    }
   },
   {
-    id: 2
+    id: 2,
+    style: {
+      opacity: 0.54
+    }
   },
   {
-    id: 3
+    id: 3,
+    style: {
+      opacity: 0.39
+    }
   },
   {
-    id: 4
-  }
+    id: 4,
+    style: {
+      opacity: 0.72
+    }
+  },
+  {
+    id: 5,
+    style: {
+      opacity: 1
+    }
+  },
+  {
+    id: 6,
+    style: {
+      opacity: 0.54
+    }
+  },
 ];
 
 export default function(props) {
@@ -42,7 +71,7 @@ export default function(props) {
       <Row style={{padding: 0, marginLeft: 0, marginRight: 0}}>
       { tilesData.map((tile) =>
         <Col xs={6} md={3} key={tile.id} style={{padding: 0}}>
-          <ResponsiveTile data={tile.data} />
+          <ResponsiveTile data={tile} />
         </Col>
       )}
       </Row>
